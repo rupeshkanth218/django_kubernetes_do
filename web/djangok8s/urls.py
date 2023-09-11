d85fecd7d9e672377a7894e2b4887f91943a7869
rupeshkanth218 <rupeshkanth218@gmail.com>
2023-09-11 10:36:47 +0530

    revamped the ui

--- web/djangok8s/urls.py
+++ web/djangok8s/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
-    path("about",views.about,name = "about")
+    path("about",views.about,name = "about"),
+    path("blog",views.blog,name = "blog"),
 ]
